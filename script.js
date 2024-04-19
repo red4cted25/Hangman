dea2655e7e5126e52d9f3e073cb2b09641358477
Nico <ndiaz413@west-mec.org>
2024-04-18 18:21:29 -0700

    Created Hangman and Design
    Created the hangman base in JS and HTML and began a Stardew Valley theme

---
+++ script.js
@@ -0,0 +1,78 @@
+$(function() {
+    // Array of words for the game
+    let words = ['Alex', 'Elliott', 'Harvey', 'Sam', 'Sebastian', 'Shane', 'Abigail', 'Emily', 'Haley', 'Leah', 'Maru', 'Penny', 'Caroline', 'Clint', 'Demetrius', 'Dwarf', 'Evelyn', 'George', 'Gus', 'Jas', 'Jodi', 'Kent', 'Krobus', 'Leo', 'Lewis', 'Linus', 'Marnie', 'Pam', 'Pierre', 'Robin', 'Sandy', 'Vincent', 'Willy', 'Wizard', 'Birdie', 'Gil', 'Governor', 'Grandpa', 'Gunther', 'Marlon', 'Morris']
+
+    // Choose random word using index
+    let chosenWord = words[Math.floor(Math.random()*words.length)];
+    let guessedLetters = [];
+    let remainingGuesses = 6;
+
+    // Display underscores for each letter of the chosen word
+    for(let i = 0; i < chosenWord.length; i++) {
+        $('#word-container').append('<div class="hidden-letter">_</div>');
+    }
+
+    // Function to update the diaplay of the guessed letters
+    function updateGuesses() {
+        $('#guess-container').empty()
+        $('#guess-container').text("Guessed Letters: " + guessedLetters.join(", "));
+    }
+
+    // Function to check if the guess letter is in the chosen word
+    function checkGuess(letter) {
+        if(chosenWord.indexOf(letter) === -1) {
+            remainingGuesses--;
+            $('#remaining-guesses').text("Remaining Guesses: " + remainingGuesses)
+        } else {
+            //Reveal the guessed letter
+            $('.hidden-letter').each(function(index) {
+                if(chosenWord[index] === letter) {
+                    $(this).text(letter)
+                }
+            })
+        }
+        updateGuesses()
+        checkGameStatus()
+    }
+
+    // FUnction to check if the game as been won or lost
+    function checkGameStatus() {
+        if($('.hidden-letter:contains("_")').length === 0) {
+            alert('Congratulations! You Won!')
+            resetGame()
+        } else if(remainingGuesses === 0) {
+            alert('You lost. Try Again! The word was: ' + chosenWord)
+            resetGame()
+        }
+    }
+
+    // Function to reset the game
+    function resetGame() {
+        guessedLetters = [];
+        remainingGuesses = 6;
+        $('#remaining-guesses').text('Remaining Guesses: ' + remainingGuesses)
+        $('#word-container').empty()
+        chosenWord = words[Math.floor(Math.random()*words.length)];
+        for(let i = 0; i < chosenWord.length; i++) {
+            $('#word-container').append('<div class="hidden-letter">_</div>');
+        }
+        updateGuesses();
+    }
+
+    //Event handler for key presses
+    $(document).keypress(function(event) {
+        let letter = String.fromCharCode(event.which).toLowerCase();
+        if(letter.match(/[a-z]/) && guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            checkGuess(letter);
+        }
+    })
+
+    //Event handler for the reset button
+    $('#reset-button').click(function() {
+        resetGame();
+    })
+
+    // Initial display of remaining guesses
+    $('#remaining-guesses').text("Remaining Guesses: " + remainingGuesses)
+})
